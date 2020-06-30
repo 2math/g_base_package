81408c2a1c917a75886b05fb4812af2ea5e63689
Galeen <2math@mail.bg>
2020-06-30 09:24:35 +0300

    v0.1.6 SingleScrollView with minHeight option

--- lib/base/widgets/single_scroll_view.dart
+++ lib/base/widgets/single_scroll_view.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/widgets.dart';
 
-
 class SingleScrollView extends StatefulWidget {
   final Widget child;
+  ///if your content is too long set this minHeight to prevent yourself for small screens in that case you will have
+  ///scroll on them
+  final double minHeight;
 
   ///wrap child with SingleChildScrollView, but the height of the view will be as matchParent and if the keyboard
   ///appear this height will be kept and view will be scrollable
-  SingleScrollView({Key key, this.child}) : super(key: key);
+  SingleScrollView({Key key, this.child, this.minHeight = 0}) : super(key: key);
 
   @override
   _SingleScrollViewState createState() => _SingleScrollViewState();
@@ -22,7 +24,11 @@ class _SingleScrollViewState extends State<SingleScrollView> {
           child: SizedBox(
               //get parent height only first time and reuse it, this way will
               // keep same height when the keyboard is shown
-              height: _bodyHeight == 0.0 ? _bodyHeight = viewportConstraints.maxHeight : _bodyHeight,
+              height: _bodyHeight == 0.0
+                  ? _bodyHeight = viewportConstraints.maxHeight < widget.minHeight
+                      ? widget.minHeight
+                      : viewportConstraints.maxHeight
+                  : _bodyHeight,
               child: widget.child));
     });
   }
