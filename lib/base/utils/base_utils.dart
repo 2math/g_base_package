758d3c195817c2fb513c5c9ffa5dbfcad39a8df6
Galeen <2math@mail.bg>
2020-07-07 10:29:58 +0300

    v0.0.14 - number input formatter, added utils , updated SingleScrollView to have also maxHeight

--- lib/base/utils/base_utils.dart
+++ lib/base/utils/base_utils.dart
@@ -1,18 +1,45 @@
+class BaseUtils {
+  static bool isEmpty(List list) {
+    return list == null || list.isEmpty;
+  }
 
-class BaseUtils{
-    static bool isEmpty(List list){
-        return list == null || list.isEmpty;
+  static bool isNotEmpty(List list) {
+    return !isEmpty(list);
+  }
+
+  static bool isEmptyStr(String string) {
+    return string == null || string.isEmpty;
+  }
+
+  static bool isNotEmptyStr(String string) {
+    return !isEmptyStr(string);
+  }
+
+  ///sort ascended
+  static int sort(String a, String b) {
+    if (a == null && b == null) {
+      return 0;
     }
 
-    static bool isNotEmpty(List list){
-        return !isEmpty(list);
+    if (a == null) {
+      return 1;
     }
 
-    static bool isEmptyStr(String string){
-        return string == null || string.isEmpty;
+    if (b == null) {
+      return -1;
     }
 
-    static bool isNotEmptyStr(String string){
-        return !isEmptyStr(string);
+    return a.compareTo(b);
+  }
+
+  String multiSelectOptionsToString(List<String> ids, {String divider = ","}) {
+    if (ids == null || ids.isEmpty) {
+      return "";
     }
+    return ids?.reduce((value, element) => value + divider + element);
+  }
+
+  List<String> stringToMultiSelectOptions(String value, {String divider = ","}) {
+    return value?.split(divider);
+  }
 }
--- lib/base/widgets/single_scroll_view.dart
+++ lib/base/widgets/single_scroll_view.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/widgets.dart';
 
-
 class SingleScrollView extends StatefulWidget {
   final Widget child;
 
+  ///if your content is too long set this minHeight to prevent yourself for small screens in that case you will have
+  ///scroll on them
+  final double minHeight;
+  final double maxHeight;
+
   ///wrap child with SingleChildScrollView, but the height of the view will be as matchParent and if the keyboard
   ///appear this height will be kept and view will be scrollable
-  SingleScrollView({Key key, this.child}) : super(key: key);
+  SingleScrollView({Key key, this.child, this.minHeight = 0, this.maxHeight = 0}) : super(key: key);
 
   @override
   _SingleScrollViewState createState() => _SingleScrollViewState();
@@ -22,8 +26,17 @@ class _SingleScrollViewState extends State<SingleScrollView> {
           child: SizedBox(
               //get parent height only first time and reuse it, this way will
               // keep same height when the keyboard is shown
-              height: _bodyHeight == 0.0 ? _bodyHeight = viewportConstraints.maxHeight : _bodyHeight,
+              height: _bodyHeight == 0.0 ? _getPrimaryHeight(viewportConstraints) : _bodyHeight,
               child: widget.child));
     });
+  }
+
+  ///get height according to current parent's height and set min/max sizes
+  double _getPrimaryHeight(BoxConstraints viewportConstraints) {
+    return _bodyHeight = viewportConstraints.maxHeight < widget.minHeight
+        ? widget.minHeight
+        : widget.maxHeight > 0 && viewportConstraints.maxHeight > widget.maxHeight
+            ? widget.maxHeight
+            : viewportConstraints.maxHeight;
   }
 }
