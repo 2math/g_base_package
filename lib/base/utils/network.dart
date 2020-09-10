4bf0e87bded9ea2e1e73640d454fc042c24c5fe7
Galeen <g.blagoev@futurist-labs.com>
2020-09-10 17:48:08 +0300

    option for multipart request without file

--- lib/base/utils/network.dart
+++ lib/base/utils/network.dart
@@ -1,4 +1,6 @@
 
+import 'dart:io';
+
 import 'package:connectivity/connectivity.dart';
 
 class NetUtil {
@@ -19,5 +21,16 @@ class NetUtil {
     Future<bool> hasNetwork() async {
         var connectivityResult = await (Connectivity().checkConnectivity());
         return connectivityResult != ConnectivityResult.none;
+    }
+
+    ///makes actual request to google.com and checks real internet connection
+    ///If you are connected to WiFi but there is no internet will return false up to 30 sec
+    Future<bool> checkInternet() async {
+      try {
+        final result = await InternetAddress.lookup('google.com');
+        return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
+      } on SocketException catch (_) {
+        return false;
+      }
     }
 }
