22088b315729e6f45ccbed8e5a967145ce23c551
Galeen <g.blagoev@bidpath.net>
2022-02-17 12:49:43 +0200

    2.0.9 - minor

--- lib/base/utils/utils.dart
+++ lib/base/utils/utils.dart
@@ -2,7 +2,7 @@ export 'dialogs.dart';
 export 'logger.dart';
 export 'validators.dart';
 export 'system.dart';
-export 'extentions.dart';
+export 'extensions.dart';
 export 'files.dart';
 export 'base_utils.dart';
 export 'network.dart';
