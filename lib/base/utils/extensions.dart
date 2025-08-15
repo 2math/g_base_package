fd9f25b70ab61df1262d654d3513022438deeadd
Galeen <criappstudio@gmail.com>
2025-08-15 11:38:51 +0300

    v3.32.2
    Fixed an issue with extension toHex on Colors that has different values now

--- lib/base/utils/extensions.dart
+++ lib/base/utils/extensions.dart
@@ -11,10 +11,10 @@ extension BaseColor on Color {
 
     /// Prefixes a hash sign if [leadingHashSign] is set to `true` (default is `true`).
     String toHex({bool leadingHashSign = true}) => '${leadingHashSign ? '#' : ''}'
-            '${a.toInt().toRadixString(16).padLeft(2, '0')}'
-            '${r.toInt().toRadixString(16).padLeft(2, '0')}'
-            '${g.toInt().toRadixString(16).padLeft(2, '0')}'
-            '${b.toInt().toRadixString(16).padLeft(2, '0')}';
+            '${(a * 255).round().toRadixString(16).padLeft(2, '0')}'
+            '${(r * 255).round().toRadixString(16).padLeft(2, '0')}'
+            '${(g * 255).round().toRadixString(16).padLeft(2, '0')}'
+            '${(b * 255).round().toRadixString(16).padLeft(2, '0')}';
 }
 
 extension BaseString on String? {
--- test/g_base_package_test.dart
+++ test/g_base_package_test.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/material.dart';
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:g_base_package/base/utils/extensions.dart';
 
 import 'package:g_base_package/g_base_package.dart';
 
@@ -10,5 +13,15 @@ void main() {
     expect(calculator.addOne(-7), -6);
     expect(calculator.addOne(0), 1);
     // expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  });
+
+  test('hex', () async {
+    String color = Color(0xFF3F3F3F).toHex(leadingHashSign: false);
+
+    expect(color, 'ff3f3f3f');
+
+    color = Color(0xFF3F3F3F).toHex();
+
+    expect(color, '#ff3f3f3f');
   });
 }
